5a23f9bc6542ccacf4a323c723747a720a75c5db
HanPham <petphamhandc@gmail.com>
2022-10-18 14:37:21 +0700

    demo thymeleaf

---
+++ src/main/java/com/codegym/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DemoController {
+    @RequestMapping("demo")
+    public ModelAndView demo(){
+        ModelAndView modelAndView = new ModelAndView("/demo");
+        return modelAndView;
+    }
+}
